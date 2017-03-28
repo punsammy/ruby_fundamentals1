dee81d6db6454ae731737088e8cda4f1da4077b4
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-03-28 08:03:05 -0400

    excerise3 complete
    exit
    d
    cd

--- excerise3.rb
+++ excerise3.rb
@@ -1,1 +1,13 @@
 puts "What is your name?"
+
+name = gets
+name = name.chomp
+
+puts "Hi #{name}!"
+
+#last step
+
+puts "How old are you?"
+number = gets.chomp.to_i
+
+puts "#{name} was born in #{2017 - number}"
